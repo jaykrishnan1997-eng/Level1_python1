27518315fd3ba3f802034c3b4bee11d884d06fd8
Jay Krishnan <jkrishna@student.42heilbronn.de>
2026-06-19 12:01:31 +0200

    completed ex5, with main

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -7,7 +7,7 @@
 #   By: jkrishna <jkrishna@student.42.fr>            +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/12 15:03:28 by jkrishna            #+#    #+#            #
-#   Updated: 2026/06/18 16:03:55 by jkrishna           ###   ########.fr      #
+#   Updated: 2026/06/19 12:00:56 by jkrishna           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -21,10 +21,11 @@ class Plant:
         self._age += 1
         
     def grow(self):
-        self._height += 2.1
+        self._height = round(self._height + 2.1, 1)
         
     def show(self):
-        print(self._name + ": " + str(self._height) + " cm, " + str(self._age) + " days old")
+        print(self._name + ": " + str(self._height) + "cm, " +
+              str(self._age) + " days old")
 
 
 class Flower(Plant):
@@ -53,7 +54,9 @@ class Tree(Plant):
 
     def produce_shade(self):
         print("[asking the " + self._name + " to produce shade]")
-        print("Tree " + self._name + " now produce a shade of " + str(self._height) + " cm long and " + str(self._trunk_diameter) + " cm wide.")
+        print("Tree " + self._name + " now produce a shade of " + 
+              str(self._height) + "cm long and " + str(self._trunk_diameter)
+              + "cm wide.")
 
     def show(self):
         super().show()
@@ -61,16 +64,40 @@ class Tree(Plant):
 
 
 class Vegetable(Plant):
-    def __init__(self, name, height, age, harvest_season: str, nutrition_value: int):
+    def __init__(self, name, height, age, harvest_season: str, n_value: int):
         super().__init__(name, height, age)
         self._harvest_season = harvest_season
-        self._nutrition_value = nutrition_value
+        self._n_value = n_value
         
+    def age(self):
+        super().age()
+
     def grow(self):
-        print("[make " + self._name + " grow and age for 20 days]")
-        self._age += 20
+        super().grow()
+        self._n_value += 1
     
     def show(self):
         super().show()
         print(" Harvest season: " + self._harvest_season)
-        print(" Nutrition value: " + self._nutrition_value)
+        print(" Nutritional value: " + str(self._n_value))
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Types ===")
+    print("=== Flower")
+    rose = Flower("Rose", 15.0, 10, "red")
+    rose.show()
+    rose.bloom()
+    rose.show()
+    print("\n=== Tree")
+    oak = Tree("Oak", 200.0, 365, 5.0)
+    oak.show()
+    oak.produce_shade()
+    print("\n=== Vegetable")
+    tomato = Vegetable("Tomato", 5.0, 10, "April", 0)
+    tomato.show()
+    print("[make " + tomato._name + " grown and age for 20 days]")
+    for i in range(20):
+        tomato.grow()
+        tomato.age()
+    tomato.show()
